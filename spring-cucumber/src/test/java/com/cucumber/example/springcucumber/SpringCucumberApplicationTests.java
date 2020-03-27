5a07ffb54fc45cb0f84ff936fe0a5e6c6863c95f
vadiraj karanam <vadi.karnam@gmail.com>
2020-03-27 20:32:57 +0530

    Basic project

---
+++ spring-cucumber/src/test/java/com/cucumber/example/springcucumber/SpringCucumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cucumber.example.springcucumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCucumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
